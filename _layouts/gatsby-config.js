978c3aa3f51bde619ad4d636d6ff55e8ba74571a
adrien.perello <adrien.perelloybestard@gmail.com>
2021-07-07 12:13:40 +0200

    add math plugin

--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -7,6 +7,8 @@ module.exports = {
     {
       resolve: `gatsby-theme-garden`,
       options: {
+        remarkPlugins: [require(`remark-math`)],
+        rehypePlugins: [require(`rehype-katex`)],
         rootNote: "/index",
         contentPath: `${__dirname}/..`,
         ignore: [
